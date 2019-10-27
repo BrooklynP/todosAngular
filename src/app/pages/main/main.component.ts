7ade452f8545afab02e3afcc254060f8797c4515
BrooklynP <brooklynpedley@gmail.com>
2019-10-27 14:13:49 +0000

    You can now have multiple lists

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
+  public selectedTodoListIndex = 0;
   public todos = [
+    {
+      name: 'todos',
+      todos: []
+    }
   ];
 
   constructor() { }
@@ -15,15 +19,28 @@ export class MainComponent implements OnInit {
   addToDo() {
     const input: any = document.getElementById('input');
     const inputText = input.value;
-    this.todos.push({name: inputText, state: 'toDo'});
+    this.todos[this.selectedTodoListIndex].todos.push({name: inputText, state: 'toDo'});
     (document.getElementById('input') as HTMLInputElement).value = '';
   }
 
   DoToDo(index: number) {
-    this.todos[index].state = 'done';
+    this.todos[this.selectedTodoListIndex].todos[index].state = 'done';
   }
 
   DeleteToDo(index: number) {
+    this.todos[this.selectedTodoListIndex].todos.splice(index, 1);
+  }
+
+  selectList(index: number) {
+    this.selectedTodoListIndex = index;
+  }
+
+  addList() {
+    this.todos.push({name: (document.getElementById('addListInput') as HTMLInputElement).value, todos: []});
+    (document.getElementById('addListInput') as HTMLInputElement).value = '';
+  }
+
+  deleteList(index: number) {
     this.todos.splice(index, 1);
   }
 
